7dbb1cd2ddbb5c1e20bd61a8b662cc76df59ff60
kgstan <chingizkg@gmail.com>
2016-01-23 20:43:40 +0530

    пофиксил коммит

--- psychic_sniffle/psychic_sniffle/urls.py
+++ psychic_sniffle/psychic_sniffle/urls.py
@@ -9,17 +9,12 @@ admin.autodiscover()
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    url(r'', include('base.urls')),
-    url(r'^place/', include('place.urls')),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^profiles/', include('profiles.urls')),
-=======
     url(r'^$', place_views.show_list, name='home'),
     url(r'^search/$', place_views.search, name='search'),
     url(r'^detail/(?P<place_slug>\S+)/$', place_views.place_detail, name='detail'),
     url(r'^category/(?P<category_slug>\S+)/$', place_views.category_list, name='category'),
->>>>>>> f8389f28cdae36e0ae6aa5c4120d45138ed265b7
     url(r'^admin/', include(admin.site.urls)),
     url(r'^redactor/', include('redactor.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +\
