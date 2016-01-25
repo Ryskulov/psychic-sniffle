2d718b39fbb0d91ab89d88ae0c7b987e5de13490
Aleksandr Sobolev <sobolev.dev@gmail.com>
2016-01-25 20:02:41 +0600

    Added signal for creating profile after user's registration

--- psychic_sniffle/profiles/models.py
+++ psychic_sniffle/profiles/models.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -18,3 +20,8 @@ class Profile(models.Model):
     def __unicode__(self):
         return self.user.username
 
+
+@receiver(post_save, sender=User, dispatch_uid="create_profile")
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
