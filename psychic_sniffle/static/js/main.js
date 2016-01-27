0201a3c64a86af3d834f4cf0d65c42ccf65600cf
kgstan <chingizkg@gmail.com>
2016-01-27 20:21:33 +0530

    рабочие закладки

--- psychic_sniffle/static/js/main.js
+++ psychic_sniffle/static/js/main.js
@@ -1,1 +1,9 @@
-
+$('.create_bookmark').on('click', function(){
+	var place_id = $(this).data('place_id');
+	$.get(
+		'/profiles/bookmarks/add/',
+		{place_id: place_id}
+	).done(function(resp){
+		alert(resp.message);
+	})
+});
